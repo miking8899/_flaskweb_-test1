b50e8e777c725602d7bd2220e49df69dad475a60
miking8899 <miking668899@gmail.com>
2023-10-03 16:19:01 +0800

    modify

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    
-    return  "<p>Hello, first test S flask World!</p><h2></h2><p> 這是另外一個段落,還有個段落</p><h3></h3><p>第2個段落</p>"
+    return render_template('index.html')
+    #return  "<p>Hello, first test S flask World!</p><h2></h2><p> 這是另外一個段落,還有個段落</p><h3></h3><p>第2個段落</p>"
 
-@app.route("/hello_world")
+@app.route("/hello")
 def hello_world():
-    return "<p>第2個網頁</p> <h1></h1><p>另一個天地,天公仔</p>"
-    
+    return render_template('home.html')
+    #return "<p>第2個網頁</p> <h1></h1><p>另一個天地,天公仔</p>"
+
     

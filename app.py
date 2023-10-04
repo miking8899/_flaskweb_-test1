69d2a16f9d9c11bf555cd363c65a2a6b781389c4
miking8899 <miking668899@gmail.com>
2023-10-05 00:31:32 +0800

    go

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask import render_template
 
 app = Flask(__name__)
 
-#print(c:\static\images\card.jpg)
+# print(c:\static\images\card.jpg)
+
+
 @app.route("/")
 def index():
     return render_template('index.html')
-    #return  "<p>Hello, first test S flask World!</p><h2></h2><p> 這是另外一個段落,還有個段落</p><h3></h3><p>第2個段落</p>"
+    # return  "<p>Hello, first test S flask World!</p><h2></h2><p> 這是另外一個段落,還有個段落</p><h3></h3><p>第2個段落</p>"
+
 
 @app.route("/hello")
 def hello_world():
     return render_template('home.html')
-    #return "<p>第2個網頁</p> <h1></h1><p>另一個天地,天公仔</p>"
-
-    
-
+    # return "<p>第2個網頁</p> <h1></h1><p>另一個天地,天公仔</p>"
